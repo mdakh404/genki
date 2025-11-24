823bf3caadb17f2907cc2a978f54a2c14655c89c
mdakh404 <akhrazmoad14@gmail.com>
2025-11-24 11:45:29 -0800

    Updating LoginController to style login fields

--- src/main/java/genki/LoginController.java
+++ src/main/java/genki/LoginController.java
@@ -1,14 +1,37 @@
 package genki;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-public class LoginController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+public class LoginController implements Initializable {
+     @FXML
+     private TextField userName;
+     @FXML
+     private TextField password;
+
+     @Override
+     public void initialize(URL location, ResourceBundle resources) {
+         userName.focusedProperty().addListener((obs, oldVal, newVal) -> {
+               if (newVal) {
+                   userName.setStyle("-fx-border-color: #374151");
+               }
+               else {
+                   userName.setStyle("-fx-border-color: #9CA3AF");
+               }
+         });
+
+         password.focusedProperty().addListener((obs, oldVal, newVal) -> {
+             if (newVal) {
+                 password.setStyle("-fx-border-color: #374151");
+             }
+             else {
+                 password.setStyle("-fx-border-color: #9CA3AF");
+             }
+         });
+     }
+
 }
