75b9dfaeca40fb5d088d8ce2116b3444c2ffff94
mdakh404 <akhrazmoad14@gmail.com>
2025-12-26 14:28:16 -0800

    Adding GroupSettingsModel.java to handle group update settings and group delete logic, adding UI components to GroupSettings.fxml

---
+++ src/main/java/genki/controllers/GroupSettingsController.java
@@ -0,0 +1,358 @@
+package genki.controllers;
+
+import genki.models.SettingsModel;
+import genki.utils.UpdateResult;
+import genki.utils.UpdateStatus;
+import genki.utils.UserSession;
+import genki.utils.DBConnection;
+import genki.utils.AlertConstruct;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+
+import java.net.URL;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.ResourceBundle;
+import java.util.Optional;
+import java.io.File;
+import java.io.IOException;
+
+
+public class GroupSettingsController implements Initializable{
+
+    private static final Logger logger = Logger.getLogger(GroupSettingsController.class.getName());
+    private static final DBConnection GSettingsControllerDBConnection = new DBConnection("genki_testing");
+    private static final SettingsModel settingsModel = new SettingsModel();
+    private boolean uploadedPhoto;
+
+    @FXML
+    private TextField newUsername;
+    @FXML
+    private PasswordField currentPassword;
+    @FXML
+    private PasswordField newPassword;
+    @FXML
+    private TextArea bioField;
+
+    @FXML
+    private Button btnSaveSettings;
+    @FXML
+    private Button btnUploadPhoto;
+    @FXML
+    private Button btnDeleteAccount;
+
+    @FXML
+    private ImageView imageProfileView;
+
+    @FXML
+    public void handleUploadPhoto() {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Profile Image");
+        FileChooser.ExtensionFilter filterImages = new FileChooser.ExtensionFilter("Image Files", "*.JPG", "*.JPEG", "*.PNG" ,"*.jpg", "*.jpeg", "*.png");
+        fileChooser.getExtensionFilters().add(filterImages);
+
+        Stage stage = (Stage) btnUploadPhoto.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        if (selectedFile != null) {
+
+            Image newImage = new Image(selectedFile.toURI().toString());
+            imageProfileView.setImage(newImage);
+
+            UpdateResult photoUploadResult = settingsModel.updatePhoto(UserSession.getUsername(), selectedFile);
+
+            if (photoUploadResult.getStatus() == UpdateStatus.PHOTO_UPDATED) {
+                uploadedPhoto = true;
+
+                String uploadedPhotoURL = settingsModel.getUploadedPhotoURL();
+
+                Image uploadedPhoto = new Image(uploadedPhotoURL, true);
+                imageProfileView.setImage(uploadedPhoto);
+            }
+
+            if (photoUploadResult.getStatus() == UpdateStatus.IMG_UPLOAD_ERROR) {
+                logger.log(Level.WARNING, "Error opening file");
+                AlertConstruct.alertConstructor(
+                        "Upload Failed",
+                        "File Upload Error",
+                        "Error while uploading file.",
+                        AlertType.ERROR
+                );
+            }
+
+        }
+
+
+    }
+
+    @FXML
+    public void handleSaveSettings() {
+
+        logger.log(Level.INFO, "Saving settings");
+
+        String newUsernameText = newUsername.getText().trim();
+        String currentPasswordText = currentPassword.getText().trim();
+        String newPasswordText = newPassword.getText().trim();
+        String bioText = bioField.getText().trim();
+
+        if (newUsernameText.isEmpty()) {
+            logger.log(Level.WARNING, "newUsername field is empty.");
+            newUsername.setStyle("-fx-border-color: #FF6347");
+        }
+
+        else {
+            UpdateResult updateUsernameResult = settingsModel.updateUsername(UserSession.getUsername(), newUsernameText);
+
+            switch (updateUsernameResult.getStatus()) {
+                case UpdateStatus.INVALID_USERNAME:
+                    logger.log(Level.WARNING, "Invalid username supplied " + newUsernameText);
+                    AlertConstruct.alertConstructor(
+                            "Settings error",
+                            "Invalid username",
+                            "Your username must be 5–15 characters long and can only include letters, numbers, and underscores.",
+                            AlertType.ERROR
+                    );
+                    break;
+
+                case UpdateStatus.USERNAME_UPDATED:
+                    UserSession.setUsername(newUsernameText);
+                    break;
+
+                case UpdateStatus.DB_ERROR:
+                    AlertConstruct.alertConstructor(
+                            "Network Error",
+                            "Database Connection Error",
+                            "Failed to connect to database, please try again in a few minutes.",
+                            AlertType.ERROR
+                    );
+                    break;
+
+                default:
+                    AlertConstruct.alertConstructor(
+                            "Unexpected Error",
+                            "Something went wrong",
+                            "An unexpected error occurred, please ty again in a few minutes.",
+                            AlertType.ERROR
+                    );
+            }
+        }
+
+        if (currentPasswordText.isEmpty()) {
+            logger.log(Level.WARNING, "currentPassword field is empty.");
+            currentPassword.setStyle("-fx-border-color: #FF6347");
+        }
+
+        if (newPasswordText.isEmpty()) {
+            logger.log(Level.WARNING, "newPassword field is empty.");
+            newPassword.setStyle("-fx-border-color: #FF6347");
+        }
+
+        else {
+            if (currentPasswordText.isEmpty()) {
+                AlertConstruct.alertConstructor(
+                        "Settings Error",
+                        "Saving Settings Failed",
+                        "Please enter your current password.",
+                        AlertType.ERROR
+                );
+            }
+            else {
+                UpdateResult updatePasswordResult = settingsModel.updatePassword(
+                        UserSession.getUsername(),
+                        currentPasswordText,
+                        newPasswordText);
+
+                switch (updatePasswordResult.getStatus()) {
+
+                    case UpdateStatus.INVALID_CURRENT_PASSWORD:
+                        AlertConstruct.alertConstructor(
+                                "Settings Error",
+                                "Incorrect current password",
+                                "The current password entered is incorrect.",
+                                AlertType.ERROR
+                        );
+                        break;
+
+                    case UpdateStatus.INVALID_NEW_PASSWORD:
+                        AlertConstruct.alertConstructor(
+                                "Settings Error",
+                                "Invalid new password",
+                                "Your new password needs at least 8 characters, with at least one uppercase letter and one symbol.",
+                                AlertType.ERROR
+                        );
+                        break;
+
+                    case UpdateStatus.PASSWORD_UPDATED:
+                        AlertConstruct.alertConstructor(
+                                "Saving Settings",
+                                "Password Updated",
+                                "Your password has been updated successfully.",
+                                AlertType.INFORMATION
+                        );
+                        break;
+
+                    case UpdateStatus.DB_ERROR:
+                        AlertConstruct.alertConstructor(
+                                "Network Error",
+                                "Database Connection Error",
+                                "Failed to connect to database, please try again in a few minutes.",
+                                AlertType.ERROR
+                        );
+                        break;
+
+                    default:
+                        AlertConstruct.alertConstructor(
+                                "Unexpected Error",
+                                "Something went wrong",
+                                "An unexpected error occurred, please ty again in a few minutes.",
+                                AlertType.ERROR
+                        );
+
+                }
+            }
+        }
+
+        if (bioText.isEmpty()) {
+            logger.log(Level.WARNING, "bioField field is empty.");
+            bioField.setStyle("-fx-border-color: #FF6347");
+        }
+
+        else {
+
+            UpdateResult updateBioResult = settingsModel.updateBio(UserSession.getUsername(), bioText);
+
+            if (updateBioResult.getStatus() == UpdateStatus.BIO_UPDATED) {
+                AlertConstruct.alertConstructor(
+                        "Saving Settings",
+                        "Bio Updated",
+                        "Your bio has been updated successfully.",
+                        AlertType.INFORMATION
+                );
+            }
+
+            else {
+                AlertConstruct.alertConstructor(
+                        "Unexpected Error",
+                        "Something went wrong",
+                        "An unexpected error occurred updating your bio, please ty again in a few minutes.",
+                        AlertType.ERROR
+                );
+            }
+        }
+
+        if (newUsernameText.isEmpty() &&
+                (newPasswordText.isEmpty() || currentPasswordText.isEmpty()) &&
+                bioText.isEmpty() && !uploadedPhoto
+        ) {
+            AlertConstruct.alertConstructor(
+                    "Settings Error",
+                    "Saving Settings Failed",
+                    "Please fill the necessary fields to update your settings.",
+                    AlertType.ERROR
+            );
+        }
+
+
+
+    }
+
+
+    @FXML
+    public void handleDeleteAccount() {
+
+        Alert deleteAccountAlert = new Alert(AlertType.CONFIRMATION);
+        deleteAccountAlert.setTitle("Confirmation");
+        deleteAccountAlert.setHeaderText("Delete account confirmation");
+        deleteAccountAlert.setContentText("Are you sure you want to delete your account ?");
+
+        Optional<ButtonType> deleteAccAlertResult = deleteAccountAlert.showAndWait();
+
+        if (deleteAccAlertResult.isPresent() && deleteAccAlertResult.get() == ButtonType.OK) {
+
+            UpdateResult deleteAccResult = settingsModel.deleteAccount(UserSession.getUsername());
+
+            switch (deleteAccResult.getStatus()) {
+
+                case UpdateStatus.ACCOUNT_DELETED:
+                    UserSession.logout();
+                    try {
+                        Stage thisStage = (Stage) btnDeleteAccount.getScene().getWindow();
+                        thisStage.close();
+                        ScenesController.switchToScene("/genki/views/Login.fxml", "Genki - Sign in");
+                    } catch (IOException e) {
+                        logger.log(Level.WARNING, "Failed to switch to Genki - Sign in");
+                    }
+                    break;
+
+                case UpdateStatus.ACCOUNT_DELETION_ERROR:
+                    AlertConstruct.alertConstructor(
+                            "Settings Error",
+                            "Account deletion error",
+                            "Failed to delete your account, please try again in a few minutes.",
+                            AlertType.ERROR
+                    );
+                    break;
+
+                case UpdateStatus.DB_ERROR:
+                    AlertConstruct.alertConstructor(
+                            "Network Error",
+                            "Database Connection Error",
+                            "Failed to connect to database, please try again in a few minutes.",
+                            AlertType.ERROR
+                    );
+                    break;
+
+                default:
+                    AlertConstruct.alertConstructor(
+                            "Unexpected Error",
+                            "Something went wrong",
+                            "An unexpected error occurred, please ty again in a few minutes.",
+                            AlertType.ERROR
+                    );
+
+
+            }
+        }
+    }
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        MongoCollection<Document> usersCollection = SettingsController.SettingsControllerDBConnection.getCollection("users");
+        Document userDoc = usersCollection.find(Filters.eq("username", UserSession.getUsername())).first();
+
+        if (userDoc != null) {
+            String photoUrl = userDoc.getString("photo_url");
+
+            if (photoUrl != null && !photoUrl.isBlank()) {
+                Image initPhotoURL = new Image(photoUrl, true);
+                imageProfileView.setImage(initPhotoURL);
+            }
+        }
+
+    }
+
+
+}
---
+++ src/main/java/genki/models/GroupSettingsModel.java
@@ -0,0 +1,162 @@
+package genki.models;
+
+import genki.utils.UpdateResult;
+import genki.utils.UpdateStatus;
+import genki.utils.DBConnection;
+import genki.utils.PasswordHasher;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.MongoException;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Map;
+import java.io.File;
+
+public class GroupSettingsModel {
+
+    private static final Dotenv env = Dotenv.load();
+    private static final Cloudinary cloudinary = new Cloudinary(env.get("CLOUDINARY_URL"));
+    private static final Logger logger = Logger.getLogger(GroupSettingsModel.class.getName());
+    private static final DBConnection GroupSettingsDBConnection = new DBConnection("genki_testing");
+    private String uploadedPhotoURL;
+
+    public String getUploadedPhotoURL() {
+        return uploadedPhotoURL;
+    }
+
+
+
+    public UpdateResult updatePhoto(String groupId, File imageFile)  {
+        logger.log(Level.INFO, "Uploading group image to Cloudinary ...");
+        try {
+            Map<?, ?> result = GroupSettingsModel.cloudinary.uploader().upload(
+                    imageFile,
+                    ObjectUtils.asMap(
+                            "folder", "genki_production/group_images",
+                            "resource_type", "image"
+                    )
+            );
+
+            MongoCollection<Document> groupsCollection = GroupSettingsDBConnection.getCollection("groups");
+            logger.log(Level.INFO, "Updating group profile_picture field ...");
+
+            uploadedPhotoURL = result.get("secure_url").toString();
+
+            groupsCollection.updateOne(
+                    Filters.eq("_id", new ObjectId(groupId)),
+                    Updates.set("profile_picture", uploadedPhotoURL)
+            );
+
+            logger.log(Level.INFO, "Updated profile_picture field.");
+            return new UpdateResult(UpdateStatus.PHOTO_UPDATED);
+
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error uploading image to Cloudinary.", e);
+            return new UpdateResult(UpdateStatus.IMG_UPLOAD_ERROR);
+        }
+    }
+
+    public UpdateResult updateGroupName(String groupId, String newGroupName) {
+
+            try {
+
+                MongoCollection<Document> groupsCollection = GroupSettingsDBConnection.getCollection("groups");
+
+                groupsCollection.updateOne(
+                        Filters.eq("_id", new ObjectId(groupId)),
+                        Updates.set("group_name", newGroupName)
+                );
+
+                logger.log(Level.INFO, "Updated group name " + " to " + newGroupName);
+                return new UpdateResult(UpdateStatus.GROUP_NAME_UPDATED);
+
+            } catch (MongoException mongoExc) {
+                logger.log(Level.WARNING, "Error updating group name ", mongoExc);
+                return new UpdateResult(UpdateStatus.DB_ERROR);
+            }
+    }
+
+    public UpdateResult updateGroupDescription(String groupId, String newDescription) {
+
+        try {
+
+            MongoCollection<Document> groupsCollection = GroupSettingsDBConnection.getCollection("groups");
+
+            groupsCollection.updateOne(
+                    Filters.eq("_id", new ObjectId(groupId)),
+                    Updates.set("description", newDescription)
+            );
+
+            logger.log(Level.INFO, "Updated group description ...");
+            return new UpdateResult(UpdateStatus.GROUP_DESCRIPTION_UPDATED);
+
+
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error updating group description ", e);
+            return new UpdateResult(UpdateStatus.DB_ERROR);
+        }
+
+    }
+
+    public UpdateResult updateVisibility(String groupId, String newVisibility) {
+
+
+            try {
+
+                MongoCollection<Document> groupsCollection = GroupSettingsDBConnection.getCollection("groups");
+
+                groupsCollection.updateOne(
+                            Filters.eq("_id", new ObjectId(groupId)),
+                            Updates.set("is_public", newVisibility.equals("Public"))
+                    );
+
+                    logger.log(Level.INFO, "Updated group visibility.");
+                    return new UpdateResult(UpdateStatus.GROUP_VISIBILITY_UPDATED);
+
+
+            } catch (MongoException mongoExc) {
+                logger.log(Level.WARNING, "Error updating group visibility ", mongoExc);
+                return new UpdateResult(UpdateStatus.DB_ERROR);
+            } catch (NullPointerException nullExcept) {
+                logger.log(Level.WARNING, "Error while accessing groups collection", nullExcept);
+                return new UpdateResult(UpdateStatus.DB_ERROR);
+            }
+
+
+    }
+
+    public UpdateResult deleteGroup(String groupId) throws MongoException {
+
+        try {
+
+            MongoCollection<Document> groupsCollection = GroupSettingsDBConnection.getCollection("groups");
+
+            DeleteResult deleteResult = groupsCollection.deleteOne(
+                    Filters.eq("_id", new ObjectId(groupId))
+            );
+
+            if (deleteResult.getDeletedCount() > 0) {
+                return new UpdateResult(UpdateStatus.GROUP_DELETED);
+            }
+            else {
+                return new UpdateResult(UpdateStatus.GROUP_DELETION_ERROR);
+            }
+
+        } catch (MongoException mongoExc) {
+            logger.log(Level.WARNING, "DB error while deleting group", mongoExc);
+            return new UpdateResult(UpdateStatus.DB_ERROR);
+        }
+    }
+
+}
